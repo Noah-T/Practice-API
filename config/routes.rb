0e115e2422037b7d7dc37669a900ecb88b0741af
Noah Teshu <nteshu@gmail.com>
2015-05-17 17:10:43 -0400

    added support for versioning to api

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  namespace :api, defaults: { format: :json } do
+  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
     
+    scope module: :v1 do
+      # We are going to list our resources here
+    end
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
