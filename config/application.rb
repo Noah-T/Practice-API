0986507cdb2da9fcca0882143dcf847b20d67f25
Noah Teshu <nteshu@gmail.com>
2015-05-19 18:30:03 -0400

    make rspec tests for api version handling pass

--- config/application.rb
+++ config/application.rb
@@ -6,18 +6,9 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-# don't generate RSpec tests for views and helpers
-  config.generators do |g|
-    g.test_framework :rspec, fixture: true
-    g.fixture_replacement :factory_girl, dir: 'spec/factories'
-    g.view_specs false
-    g.helper_specs false
-    g.stylesheets = false
-    g.javascripts = false
-    g.helper = false
-  end
 
-  config.autoload_paths += %W(\#{config.root}/lib)
+
+  
 
 module MarketPlaceApi1
   class Application < Rails::Application
@@ -32,8 +23,18 @@ module MarketPlaceApi1
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    # don't generate RSpec tests for views and helpers
+  config.generators do |g|
+    g.test_framework :rspec, fixture: true
+    g.fixture_replacement :factory_girl, dir: 'spec/factories'
+    g.view_specs false
+    g.helper_specs false
+    g.stylesheets = false
+    g.javascripts = false
+    g.helper = false
+  end
 
-    # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
+  config.autoload_paths += %W(\#{config.root}/lib)
+
   end
 end
